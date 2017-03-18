006107e3fcf3b3bb8dd9d39462b0fecb07ff1928
Khanpara <khanpar@amazon.com>
2017-03-19 00:12:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/convergeconsult/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.example.convergeconsult;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by khanpar on 3/18/17.
+ */
+
+public class AboutActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+    }
+}
